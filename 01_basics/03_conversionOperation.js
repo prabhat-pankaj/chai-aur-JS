8d51f7410e6b3574d0cd6817a11c028d9ae0df8d
Prabhat Kumar Pankaj <143256704+prabhat-pankaj@users.noreply.github.com>
2024-04-02 14:19:07 +0000

    Data Type Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = "50" 
+// let score = "50abs" // NaN
+// let score = null  // 0
+// let score = undefined // NaN
+// let score = true  // 1
+// let score = false  // 0
+let score = "prabhat" //NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;  false => 0
+
+
+// let isLoggedIn = 1  // true
+// let isLoggedIn = ""   // false
+// let isLoggedIn = " " // true
+// let isLoggedIn = "prabhat" // true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;  0 => false
+// "" => false;  " " => true
+// "Prabhat" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
