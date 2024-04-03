0d916ce92659f05e8e488a58bff4f52550f09944
Prabhat Kumar Pankaj <143256704+prabhat-pankaj@users.noreply.github.com>
2024-04-03 10:48:39 +0000

    Arithmetic Operators

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,36 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+
+let str1 = "Hello"
+let str2 = " Prabhat"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((4 + 6) * 5 / 10)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter)
+
