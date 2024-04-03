139802758ba60e88dd68262040b47fd1b76c0bfe
Prabhat Kumar Pankaj <143256704+prabhat-pankaj@users.noreply.github.com>
2024-04-03 10:54:10 +0000

    Arithmetic Operators and Comparison Operators

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -34,3 +34,28 @@ let gameCounter = 100
 ++gameCounter
 console.log(gameCounter)
 
+
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
