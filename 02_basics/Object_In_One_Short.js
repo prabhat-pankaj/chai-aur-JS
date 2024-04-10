183bb3b823b9cf71eef9e40121def4b91659323f
Prabhat Kumar Pankaj <143256704+prabhat-pankaj@users.noreply.github.com>
2024-04-10 11:09:30 +0000

    Object

--- 02_basics/Object_In_One_Short.js
+++ 02_basics/Object_In_One_Short.js
@@ -38,4 +38,80 @@ mySchool = {
 const object1 = new Object();
 object1.property1 = 42;
 
-console.log(object1.hasOwnProperty('property1'));
+// console.log(object1.hasOwnProperty('property1'));
+
+
+
+// Accessing object members
+mySchool = {
+    name: 'Vivekanada',
+    location: 'Delhi',
+    established: 1971,
+    20 : 1000,
+    displayInfo: function() {
+        console.log(`${school.name} was established in ${school.established} at ${school.location}`)
+    }
+}
+// console.log(mySchool.name)
+
+// console.log(mySchool.established);
+
+
+
+// Bracket Notation
+mySchool = {
+    name: 'Vivekanada School',
+    location: 'Delhi',
+    established: 1995,
+    20: 1000,
+    displayInfo: function() {
+        document.write(`${school.name} was established in ${school.established} at ${school.location}`)
+    }
+}
+
+// console.log(mySchool['name']);
+
+// console.log(mySchool['20']);
+
+
+
+let person = {
+    gender: 'male'
+}
+
+let person1 = Object.create(person)
+person1.name = 'Prabaht'
+person1.age = 21
+person1.nationality = 'Indian'
+
+for (let key in person1) {
+    // console.log(key);
+}
+
+
+// Deleting Properties 
+// let obj1 = {
+//     propFirst : 'Name'
+// }
+
+// console.log(obj1.propFirst);
+// delete obj1.propFirst
+
+// console.log(obj1.propFirst);
+
+
+
+// we can not delete inherited properties or non-configurable properties
+
+let obj1 = {
+    propfirst: 'Name'
+}
+console.log(obj1.propfirst);  // Name
+let obj2 = Object.create(obj1)
+
+console.log(obj2.propfirst);  // Name
+
+console.log(delete obj2.propfirst);  // true
+    // Surprisingly Note that this will return true 
+    // regardless of whether the deletion was successful
+    console.log(obj2.propfirst);  // Name
