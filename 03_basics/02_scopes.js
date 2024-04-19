a0a017404ad6f04c058eae28a7cbc45dd79045be
Prabhat Kumar Pankaj <143256704+prabhat-pankaj@users.noreply.github.com>
2024-04-19 10:19:39 +0000

    Scope level and Mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,22 @@ let a = 300
 if (true) {
     let a = 10;
     const b = 20;
-    console.log('INNER: ', a);
+    // console.log('INNER: ', a);
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "prabhat"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+one()
