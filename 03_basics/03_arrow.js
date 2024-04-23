3e22277bc7413554ba2153385e7ffa43a0907f07
Prabhat Kumar Pankaj <143256704+prabhat-pankaj@users.noreply.github.com>
2024-04-23 05:48:49 +0000

    Arrow Function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -13,3 +13,40 @@ const user ={
 // user.welcomeMessage()
 
 // console.log(this);
+
+
+
+// function chai() {
+//     let username = "Prabhat"
+//     console.log(this.username);
+// }
+// chai()
+
+
+
+// const chai = function () {
+//     let username = "Prabhat"
+//     console.log(this.username);
+// }
+
+
+const chai =  () => {
+    let username = "Prabhat"
+    console.log(this);
+}
+
+// chai()
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "Prabhat"})
+
+
+console.log(addTwo(10, 20))
